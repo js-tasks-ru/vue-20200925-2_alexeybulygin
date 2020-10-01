03ae4ee182a31fdf8efe70ad5e249485d09efddf
Alexey Bulygin <mdss@makexhtml.ru>
2020-10-01 18:22:17 +0300

    done CounterButton task

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,17 @@
 import Vue from './vue.esm.browser.js';
 
+const app = new Vue({
+  el: '#counter',
+  data: {
+    value: 0,
+  },
+
+  methods: {
+    onClick() {
+      this.value++;
+    }
+  }
+})
+
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
