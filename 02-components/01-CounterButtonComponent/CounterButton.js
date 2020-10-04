4c316b55f1b23554906e4ae0e07bebba13fcb094
Alexey Bulygin <mdss@makexhtml.ru>
2020-10-04 16:19:03 +0300

    CounterButton неполное решение

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,7 +1,28 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" :count="count" @click="increment(count)">{{ count }}</button>',
 
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+
+  },
+
+  model: {
+    prop: 'count',
+    event: 'click',
+  },
+
+  methods: {
+    increment(count) {
+      let newValue = count;
+      newValue++;
+      this.$emit('click', newValue);
+    }
+  },
   // Компонент должен иметь входной параметр
 
   // Компонент должен иметь модель
