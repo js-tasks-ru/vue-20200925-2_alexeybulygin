a275aad22affc215396714c1d4eb534e856d5478
Alexey Bulygin <mdss@makexhtml.ru>
2020-10-21 10:21:18 +0300

    done AuthPages task

--- 04-SPA/05-AuthPages/views/LoginPage.vue
+++ 04-SPA/05-AuthPages/views/LoginPage.vue
@@ -1,12 +1,56 @@
 <template>
-  <form class="form"></form>
+  <form class="form" id="login-form" @submit.prevent="loginForm">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input type="email" name="email" v-model="email" placeholder="demo@email" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input type="password" name="password" v-model="password" placeholder="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary button_block">Войти</button>
+    </div>
+    <div class="form__append">Нет аккаунта? <router-link :to="{ name: 'register' }" class="link">Зарегистрируйтесь</router-link></div>
+  </form>
 </template>
 
 <script>
-// import { login } from '../data';
+import { login } from '../data';
 
 export default {
   name: 'LoginPage',
+
+  data() {
+    return {
+      email: null,
+      password: null,
+    }
+  },
+
+  methods:{
+    loginForm:function() {
+      if(this.email && this.password) {
+        this.getUser();
+      }
+      if(!this.email) alert("Требуется ввести Email");
+      if(!this.password) alert("Требуется ввести пароль");
+    },
+
+    async getUser() {
+      let response = await login(this.email, this.password);
+      if (response.statusCode === 403) {
+        alert('Неверные учетные данные');
+      } else {
+        alert(response.fullname);
+      }
+    }
+  },
+
 };
 </script>
 
--- 04-SPA/05-AuthPages/views/RegisterPage.vue
+++ 04-SPA/05-AuthPages/views/RegisterPage.vue
@@ -1,12 +1,78 @@
 <template>
-  <form class="form"></form>
+  <form class="form" id="register-form" @submit.prevent="registerForm">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input type="email" name="email" v-model="email" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Имя</label>
+      <div class="input-group">
+        <input type="text" name="username" v-model="username" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input type="password" name="password" v-model="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Повтор пароля</label>
+      <div class="input-group">
+        <input type="password" name="passwordRepeat" v-model="passwordRepeat" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="checkbox"><input type="checkbox" name="license" v-model="license" /> Я согласен с условиями <span></span></label>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary">Зарегистрироваться</button>
+    </div>
+    <div class="form__append">Уже есть аккаунт? <router-link :to="{ name: 'login' }" class="link">Войдите</router-link></div>
+  </form>
 </template>
 
 <script>
-// import { register } from '../data';
+import { login, register } from '../data';
 
 export default {
   name: 'RegisterPage',
+
+  data() {
+    return {
+      email: null,
+      username: null,
+      password: null,
+      passwordRepeat: null,
+      license: null,
+    }
+  },
+
+  methods:{
+    registerForm:function(e) {
+      if(this.email && this.username && this.password && this.passwordRepeat && this.license) {
+        this.regUser();
+      }
+      if(!this.email) alert("Требуется ввести Email");
+      if(!this.username) alert("Требуется ввести полное имя");
+      if(!this.password) alert("Требуется ввести пароль");
+      if(!this.passwordRepeat) alert("Требуется ввести пароль еще раз");
+      if(this.passwordRepeat !== this.password) alert("Пароли не совпадают");
+      if(!this.license) alert("Требуется согласиться с условиями");
+    },
+
+    async regUser() {
+      let response = await register(this.email, this.username, this.password);
+      if (response.id) {
+        alert(response.id);
+      } else {
+        alert(response.message);
+      }
+    }
+  },
+
 };
 </script>
 
